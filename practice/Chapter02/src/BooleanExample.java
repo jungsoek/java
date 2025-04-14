29cb4e2c6681a4650f8c94ef2fb7996bf8229370
jungseok <jungsoeko@gmail.com>
2025-04-15 02:09:11 +0900

    2장 변수와 타입

---
+++ practice/Chapter02/src/BooleanExample.java
@@ -0,0 +1,23 @@
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean stop = true;
+		
+		if(stop) {
+			System.out.println("stop");
+		} else {
+			System.out.println("start");
+		}
+		
+		int x = 10;
+		boolean result1 = (x == 20);
+		boolean result2 = (x != 20);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+
+	}
+
+}
